a2491674f1c587d13978a12cad14ac6f0629648f
raksgodi <drrakeshkumargodi@gmail.com>
2022-09-11 12:03:32 +0530

    heklo

--- bash/front.sh
+++ bash/front.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e   # ensure your script will stop if any of the instruction fails
 
-source components/common.sh
+#source components/common.sh
 
 echo -n "Installing Nginx: "
 yum install nginx -y   >> /tmp/frontend.log 
